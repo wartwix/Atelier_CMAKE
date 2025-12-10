6fac4b4e8caa4cdf6eb94c088df66b03aacca396
wartwix <n.sadra1990@gmail.com>
2025-12-10 10:14:52 +0100

    Enhance main function to handle 'car' operation
    Added support for a new operation 'car' with two arguments.

--- src/main.c
+++ src/main.c
@@ -3,33 +3,53 @@
 #include <string.h>
 #include "./../include/calculator.h"
 
-int main(int argc, char *argv[]){
-	if (argc == 4)
-	{
-	char* op = argv[1];
-	char* a = argv[2];
-	char* b = argv[3];
-	double r = 0;
-	if (strcmp(op, "add") == 0) {
-		r = _add(atof(a), atof(b));
-		printf("%lf",r);
-	}
-	else if (strcmp(op, "sub") == 0) {
-		r = _sub(atof(a), atof(b));
-		printf("%lf",r);
-	}
-	else if (strcmp(op, "mul") == 0) {
-		r = _mul(atof(a), atof(b));
-		printf("%lf",r);
-	}
-	else if (strcmp(op, "div") == 0) {
-		r = _div(atof(a), atof(b));
-		printf("%lf",r);
-	}
-	else {printf("Erreur de parametres");}
-	}
-	else {printf("Erreur de parametres");}
-	return 0;
-}
+int main(int argc, char *argv[])
+{
+    char *op;
+    char *a;
+    char *b;
+    double r = 0;
 
-		
+    if (argc == 4) {
+        op = argv[1];
+        a = argv[2];
+        b = argv[3];
+
+        if (strcmp(op, "add") == 0) {
+            r = _add(atof(a), atof(b));
+            printf("%lf", r);
+        }
+        else if (strcmp(op, "sub") == 0) {
+            r = _sub(atof(a), atof(b));
+            printf("%lf", r);
+        }
+        else if (strcmp(op, "mul") == 0) {
+            r = _mul(atof(a), atof(b));
+            printf("%lf", r);
+        }
+        else if (strcmp(op, "div") == 0) {
+            r = _div(atof(a), atof(b));
+            printf("%lf", r);
+        }
+        else {
+            printf("Erreur de parametres");
+        }
+    }
+    else if (argc == 3) {
+        op = argv[1];
+        a  = argv[2];
+
+        if (strcmp(op, "car") == 0) {
+            r = _car(atof(a));
+            printf("%lf", r);
+        }
+        else {
+            printf("Erreur de parametres");
+        }
+    }
+    else {
+        printf("Erreur de parametres");
+    }
+
+    return 0;
+}
